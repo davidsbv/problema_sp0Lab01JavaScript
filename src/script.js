522abbc0ad4cf8fa6d9d6da7f70537025f874c63
davidsbv <dev.perez.ruiz@gmail.com>
2024-03-28 07:36:50 +0100

    comienzo funcionalidad addTask sin bucle for

--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
-// Variable para contabilizar el número de tareas
-let idTask = 0;
-
 // Array para guardar las tareas
 let taskArray = [];
 
+// Variable para contabilizar el número de tareas
+let idTask = 0;
+console.log(idTask)
+let ultimaPosicion;
 //  Función constructora Obeto tarea
 function task(name){
     this.id = idTask++;
@@ -47,7 +48,13 @@ function makeTask(nameTask){
 }
 
 // Función para colocar las tareas en el documento
-function refreshTasks(){
+function addTask(){
+
+
+   //removeAll();
+   
+    
+    //taskArray.forEach(element => {
 
 
     // Etiqueta li
@@ -67,47 +74,48 @@ function refreshTasks(){
     const trashIcon = document.createElement('i');
 
 
-    for (let i = 0; i < taskArray.length; i++) {
-       
-        li.setAttribute('id', i);
-        trashIcon.setAttribute('id', i);
-        input.setAttribute('id', i)
-        input.setAttribute('type', 'checkbox'); 
-        input.classList.add('task-checkbox');
-        span.classList.add('task-text');
-        trashIcon.setAttribute('class', 'fa fa-trash');
-        console.log(span.value = taskArray[i].name);
-        span.value = taskArray[i].name;
-        span.textContent = span.value;
-        li.appendChild(article).appendChild(input);
-        li.appendChild(article).appendChild(span);
-        li.appendChild(trashIcon);
-        taskList.appendChild(li); 
 
-        console.log(input)
-        
-        input.addEventListener('click', (event) => {
-            const target = event.target;
-            if (input.checked) {
-                span.style.color = 'red'
-                span.style.textDecoration = 'line-through';
-                taskArray[target.id].done = true;
-            } else {
-                span.style.color = 'black'
-                span.style.textDecoration = 'none';
-                taskArray[target.id].done = false;
-            }
-            console.log(taskArray[i])
-        });
+  
+    
 
-        trashIcon.addEventListener('click', removeTask);
+    li.setAttribute('id', i);
+    trashIcon.setAttribute('id', i);
+    input.setAttribute('id', i)
+    input.setAttribute('type', 'checkbox'); 
+    input.classList.add('task-checkbox');
+    span.classList.add('task-text');
+    trashIcon.setAttribute('class', 'fa fa-trash');
+    //console.log(span.value = taskArray[i].name);
+    span.value = taskArray[i].name;
+    span.textContent = span.value;
+    li.appendChild(article).appendChild(input);
+    li.appendChild(article).appendChild(span);
+    li.appendChild(trashIcon);
+    taskList.appendChild(li); 
 
-        // const checked = input.checked;
-        // console.log(checked);
+    refreshTaskLenght();
+    
+    input.addEventListener('click', (event) => {
+        const target = event.target;
+        if (input.checked) {
+            span.style.color = 'red'
+            span.style.textDecoration = 'line-through';
+            span.style.textDecorationColor = 'black'
+            taskArray[target.id].done = true;
+        } else {
+            span.style.color = 'black'
+            span.style.textDecoration = 'none';
+            taskArray[target.id].done = false;
+        }
+        console.log(taskArray[i])
+    });
 
-    }
+    trashIcon.addEventListener('click', removeTask);
 
-    // input.setAttribute('type', 'checkbox'); 
+    // const checked = input.checked;
+    // console.log(checked);
+
+    input.setAttribute('type', 'checkbox'); 
     // input.classList.add('task-checkbox');
     // span.classList.add('task-text');
     // trashIcon.setAttribute('class', 'fa fa-trash');
@@ -125,9 +133,10 @@ addButton.addEventListener('click', () => {
         alert('Por favor, pon nombre a la tarea');
     } else {
         taskArray.push(makeTask(taskName.value));
-        refreshTasks();
-        //refreshTasksNumber();
+        addTask();
+        //addTaskNumber();
         cleanNameOfTask();  
+        console.log('añadiendo')
         
     }
 })
@@ -135,13 +144,41 @@ addButton.addEventListener('click', () => {
 // Borrar tarea
 function removeTask(event){
     //const idTrash = document.getElementsByClassName('fa fa-trash')
-    const trashId = event.target;
-    const li = Array.from(document.querySelectorAll('li'))
+    //const li = Array.from(document.querySelectorAll('li'))
+
+    // Elemnto que dispara el evento
+    const trashId = event.target.parentElement;
+    //const ul = document.querySelectorAll('li')
+    const taskList = document.getElementById('taskList')
+    console.log('trasId' + trashId.id)
+    if (taskList.hasChildNodes()) {
+        taskArray.splice(trashId.id)
+        taskList.removeChild(taskList.children[trashId.id])
+       
+        refreshTaskLenght();
+        //addTask();
+    } 
     
-    //taskList.removeChild(trashId);
-    //refreshTasks();
-     console.log(trashId)
-     console.log(li)
+   
+    // taskList.removeChild(li.id = trashId);
+    
+   
+   // console.log(trashId)
+     console.log(trashId.id)
+    //  console.log(li)
     
     
 }
+
+function removeAll() {
+    const taskList = document.getElementById("taskList");
+    while (taskList.hasChildNodes()) {
+      taskList.removeChild(taskList.firstChild);
+      refreshTaskLenght();
+    
+    }
+  }
+
+  function refreshTaskLenght() {
+    idTask = (taskArray.length);
+  }
